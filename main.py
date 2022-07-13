8970a77db26ebbf37988e95a4d4953345e3769d8
Yu979 <875837195@qq.com>
2022-07-13 23:21:05 +0800

    params modify9

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = '0,2'
+# os.environ["CUDA_VISIBLE_DEVICES"] = '0,2'
 import math
 import logging
 import joblib

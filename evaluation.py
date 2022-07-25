5469766192cc503ec1f1446ab9fe35c05082e4eb
Yu979 <875837195@qq.com>
2022-07-25 18:49:02 +0800

    fast tune2

--- evaluation.py
+++ evaluation.py
@@ -12,61 +12,75 @@ import parse_data as parser
 import pandas as pd
 
 
-def eval_perf(p, our_model, head, eval_infos, should_draw, current_epoch, label, one_hot):
+def eval_perf(p, our_model, head, eval_infos_all, should_draw, current_epoch, label, one_hot):
     import model as mo
     eval_track_names = []
     for key in head.keys():
         eval_track_names += head[key]
+    eval_infos = []
+    for info in eval_infos_all:
+        if info[5]:
+            continue
+        eval_infos.append(info)
 
-    if Path(f"{p.pickle_folder}{label}_seq.gz").is_file():
-        print(datetime.now().strftime('[%H:%M:%S] ') + "Loading sequences. ")
-        test_seq = joblib.load(f"{p.pickle_folder}/{label}_seq.gz")
-        print(datetime.now().strftime('[%H:%M:%S] ') + "Loading gt 1. ")
-        # eval_gt_tss = joblib.load(f"{p.pickle_folder}/{label}_eval_gt_tss.gz")
-        eval_gt_tss = pickle.load(open(f"{p.pickle_folder}/{label}_eval_gt_tss.gz", "rb"))
-        print(datetime.now().strftime('[%H:%M:%S] ') + "Finished loading. ")
-    else:
-        load_info = []
-        for j, info in enumerate(eval_infos):
-            mid = int(info[1] / p.bin_size)
-            load_info.append([info[0], mid])
-        print("Loading ground truth tracks")
-        gt = parser.par_load_data(load_info, eval_track_names, p)
-        print("Extracting evaluation regions")
-        eval_gt_tss = {}
+    # if Path(f"{p.pickle_folder}{label}_seq.gz").is_file():
+    #     print(datetime.now().strftime('[%H:%M:%S] ') + "Loading sequences. ")
+    #     test_seq = joblib.load(f"{p.pickle_folder}/{label}_seq.gz")
+    #     print(datetime.now().strftime('[%H:%M:%S] ') + "Loading gt. ")
+    #     eval_gt = joblib.load(f"{p.pickle_folder}/{label}_eval_gt.gz")
+    #     # eval_gt_tss = pickle.load(open(f"{p.pickle_folder}/{label}_eval_gt_tss.gz", "rb"))
+    #     print(datetime.now().strftime('[%H:%M:%S] ') + "Finished loading. ")
+    # else:
+    load_info = []
+    for j, info in enumerate(eval_infos):
+        mid = int(info[1] / p.bin_size)
+        load_info.append([info[0], mid])
+    print("Loading ground truth tracks")
+    gt = parser.par_load_data(load_info, eval_track_names, p)
+    print("Extracting evaluation regions")
+    eval_gt = {}
+    for i in range(len(eval_infos)):
+        eval_gt[eval_infos[i][2]] = {}
 
-        for i, info in enumerate(eval_infos):
-            for j, track in enumerate(eval_track_names):
-                eval_gt_tss.setdefault(track, []).append(gt[i, j])
+    for i, info in enumerate(eval_infos):
+        for j, track in enumerate(eval_track_names):
+            eval_gt[info[2]].setdefault(track, []).append(gt[i, j])
 
-        print("Extracting DNA regions")
-        test_seq = []
-        for info in eval_infos:
-            start = int(info[1] - (info[1] % p.bin_size) - p.half_size)
-            extra = start + p.input_size - len(one_hot[info[0]])
-            if start < 0:
-                ns = one_hot[info[0]][0:start + p.input_size]
-                ns = np.concatenate((np.zeros((-1 * start, 5)), ns))
-            elif extra > 0:
-                ns = one_hot[info[0]][start: len(one_hot[info[0]])]
-                ns = np.concatenate((ns, np.zeros((extra, 5))))
-            else:
-                ns = one_hot[info[0]][start:start + p.input_size]
-            test_seq.append(ns[:, :-1])
-        test_seq = np.asarray(test_seq, dtype=bool)
-        print(f"Length: {len(test_seq)}")
-        gc.collect()
-        print("Dumping the evaluation data")
-        # joblib.dump(test_seq, f"{p.pickle_folder}/{label}_seq.gz", compress="lz4")
-        # # pickle.dump(test_seq, open(f"{p.pickle_folder}/{chr_name}_seq.gz", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
-        # gc.collect()
-        # # joblib.dump(eval_gt_tss, f"{p.pickle_folder}/{label}_eval_gt_tss.gz", compress="lz4")
-        # pickle.dump(eval_gt_tss, open(f"{p.pickle_folder}/{label}_eval_gt_tss.gz", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+    for i, gene in enumerate(eval_gt.keys()):
+        if i % 10 == 0:
+            print(i, end=" ")
+        for track in eval_track_names:
+            eval_gt[gene][track] = np.sum(eval_gt[gene][track])
+    print("")
+
+    print("Extracting DNA regions")
+    test_seq = []
+    for info in eval_infos:
+        start = int(info[1] - (info[1] % p.bin_size) - p.half_size)
+        extra = start + p.input_size - len(one_hot[info[0]])
+        if start < 0:
+            ns = one_hot[info[0]][0:start + p.input_size]
+            ns = np.concatenate((np.zeros((-1 * start, 5)), ns))
+        elif extra > 0:
+            ns = one_hot[info[0]][start: len(one_hot[info[0]])]
+            ns = np.concatenate((ns, np.zeros((extra, 5))))
+        else:
+            ns = one_hot[info[0]][start:start + p.input_size]
+        test_seq.append(ns[:, :-1])
+    test_seq = np.asarray(test_seq, dtype=bool)
+    print(f"Length: {len(test_seq)}")
+    gc.collect()
+    # print("Dumping the evaluation data")
+    # joblib.dump(test_seq, f"{p.pickle_folder}/{label}_seq.gz", compress="lz4")
+    # # # pickle.dump(test_seq, open(f"{p.pickle_folder}/{chr_name}_seq.gz", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+    # # gc.collect()
+    # joblib.dump(eval_gt, f"{p.pickle_folder}/{label}_eval_gt.gz", compress="lz4")
+    # # pickle.dump(eval_gt_tss, open(f"{p.pickle_folder}/{label}_eval_gt_tss.gz", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
 
     start_val = {}
     track_inds_bed = []
-    meta = pd.read_csv("/data2/qinze/geneExpression/media/user/PASSPORT1/variants_100/data/ML_all_track.metadata.2022053017.tsv", sep="\t")
-    cor_tracks = pd.read_csv("/data2/qinze/geneExpression/media/user/PASSPORT1/variants_100/data/fantom_tracks.tsv", sep="\t", header=None).iloc[:, 0].tolist()
+    meta = pd.read_csv("data/ML_all_track.metadata.2022053017.tsv", sep="\t")
+    cor_tracks = pd.read_csv("data/fantom_tracks.tsv", sep="\t", header=None).iloc[:, 0].tolist()
     print(f"Number of correlation tracks: {len(cor_tracks)}")
     track_types = {}
     for track in eval_track_names:
@@ -81,7 +95,7 @@ def eval_perf(p, our_model, head, eval_infos, should_draw, current_epoch, label,
         print(w, end=" ")
         pr = our_model.predict(mo.wrap2(test_seq[w:w + p.w_step], p.predict_batch_size))
         p1 = np.concatenate((pr[0], pr[1], pr[2]), axis=1)
-        p2 = p1[:, :, p.mid_bin - 2] + p1[:, :, p.mid_bin - 1] + p1[:, :, p.mid_bin] + p1[:, :, p.mid_bin + 1] + p1[:, :, p.mid_bin + 2]
+        p2 = p1[:, :, p.mid_bin - 1] + p1[:, :, p.mid_bin] + p1[:, :, p.mid_bin + 1]
         if w == 0:
             predictions = p2
         else:
@@ -102,14 +116,21 @@ def eval_perf(p, our_model, head, eval_infos, should_draw, current_epoch, label,
         p2 = None
         predictions_for_bed = None
         gc.collect()
-    # joblib.dump(predictions, "pred.gz", compress="lz4")
-    protein_gene_set = []
+    joblib.dump(predictions, "pred.gz", compress="lz4")
+    final_pred = {}
+    for i in range(len(eval_infos)):
+        final_pred[eval_infos[i][2]] = {}
     final_pred_tss = {}
     for i in range(len(eval_infos)):
-        if not eval_infos[i][5]:
-            protein_gene_set.append(eval_infos[i][2])
         for it, track in enumerate(eval_track_names):
+            final_pred[eval_infos[i][2]].setdefault(track, []).append(predictions[i][it])
             final_pred_tss.setdefault(track, []).append(predictions[i][it])
+
+    for i, gene in enumerate(final_pred.keys()):
+        if i % 10 == 0:
+            print(i, end=" ")
+        for track in eval_track_names:
+            final_pred[gene][track] = np.sum(final_pred[gene][track])
 
     # print("Saving bed files")
     # for track in start_val.keys():
@@ -124,17 +145,17 @@ def eval_perf(p, our_model, head, eval_infos, should_draw, current_epoch, label,
     corr_p = []
     corr_s = []
     genes_performance = []
-    for i in range(len(eval_infos)):
+    for gene in final_pred.keys():
         a = []
         b = []
         indices = []
         for v, track in enumerate(eval_track_names):
-            if track not in cor_tracks:
-                continue
+            # if track not in cor_tracks:
+            #     continue
             if track_types[track] != "CAGE" or "FANTOM5" not in track:
                 continue
-            a.append(final_pred_tss[track][i])
-            b.append(eval_gt_tss[track][i])
+            a.append(final_pred[gene][track])
+            b.append(eval_gt[gene][track])
             indices.append(v)
         a = np.nan_to_num(a, neginf=0, posinf=0)
         b = np.nan_to_num(b, neginf=0, posinf=0)
@@ -143,28 +164,32 @@ def eval_perf(p, our_model, head, eval_infos, should_draw, current_epoch, label,
         if not math.isnan(sc) and not math.isnan(pc):
             corr_p.append(pc)
             corr_s.append(sc)
-            genes_performance.append(f"{eval_infos[i][2]}-{eval_infos[i][1]}\t{sc}\t{np.mean(b)}\t{np.std(b)}\t{eval_track_names[indices[np.argmin(a)]]}\t{eval_track_names[indices[np.argmax(a)]]}")
+            genes_performance.append(f"{gene}\t{sc}\t{np.mean(b)}\t{np.std(b)}\t{eval_track_names[indices[np.argmin(a)]]}\t{eval_track_names[indices[np.argmax(a)]]}")
 
     print("")
     print(f"Across tracks {len(corr_p)} {np.mean(corr_p)} {np.mean(corr_s)}")
     with open("genes_performance.tsv", 'w+') as f:
         f.write('\n'.join(genes_performance))
 
-    print("Across genes (TSS)")
+    print("Across genes")
     corrs_p = {}
     corrs_s = {}
     all_track_spearman = {}
     track_perf = {}
-    print(f"Number of TSS: {len(eval_gt_tss[eval_track_names[0]])}")
     for track in eval_track_names:
-        a = eval_gt_tss[track]
-        b = final_pred_tss[track]
+        # if track_types[track] == "CAGE" and track not in cor_tracks:
+        #     continue
+        a = []
+        b = []
+        for gene in eval_gt.keys():
+            a.append(final_pred[gene][track])
+            b.append(eval_gt[gene][track])
         a = np.nan_to_num(a, neginf=0, posinf=0)
         b = np.nan_to_num(b, neginf=0, posinf=0)
         pc = stats.pearsonr(a, b)[0]
         sc = stats.spearmanr(a, b)[0]
         track_perf[track] = pc
-        if pc is not None and sc is not None:
+        if not math.isnan(sc) and not math.isnan(pc):
             corrs_p.setdefault(track_types[track], []).append((pc, track))
             corrs_s.setdefault(track_types[track], []).append((sc, track))
             all_track_spearman[track] = stats.spearmanr(a, b)[0]
@@ -185,7 +210,7 @@ def eval_perf(p, our_model, head, eval_infos, should_draw, current_epoch, label,
     return_result = np.mean([i[0] for i in corrs_s["CAGE"]])
 
     if should_draw:
-        viz.draw_regplots(eval_track_names, track_perf, final_pred_tss, eval_gt_tss,
+        viz.draw_regplots(eval_track_names, track_perf, final_pred_tss, eval_gt,
                           f"{p.figures_folder}/plots/epoch_{current_epoch}")
 
     return return_result
--- model.py
+++ model.py
@@ -66,9 +66,9 @@ def make_head(track_num, num_regions, output1d, name):
 
 
 def resnet(input_x, input_size):
-    print("Version 1.05")
+    print("Version 1.10")
     # Initial number of filters
-    num_filters = 512
+    num_filters = 768
     mlp_start_block = 6
     num_blocks = 12
     patchify_val = 4
@@ -93,7 +93,7 @@ def resnet(input_x, input_size):
         y = x
         # y = GaussianNoise(0.001)(y)
         y = GaussianDropout(0.001)(y)
-        y1 = DepthwiseConv1D(kernel_size=9, name=cname + "depthwise", padding="same")(y)
+        y1 = DepthwiseConv1D(kernel_size=7, name=cname + "depthwise", padding="same")(y)
         # Spatial MLP for long range interactions
         if block >= mlp_start_block:
             y2 = tf.transpose(y, [0, 2, 1])
@@ -123,7 +123,7 @@ def fast_mse(y_true, y_pred):
     y_true_positive = tf.gather_nd(y_true, tf.where(y_true > 0))
     non_zero_mse = tf.reduce_mean(tf.square(y_true_positive - y_pred_positive))
     non_zero_mse = tf.where(tf.math.is_nan(non_zero_mse), tf.zeros_like(non_zero_mse), non_zero_mse)
-    total_loss = normal_mse + 0.2 * non_zero_mse
+    total_loss = normal_mse + 0.3 * non_zero_mse
     return total_loss
 
 
@@ -229,222 +229,3 @@ def exponential_linspace_int(start, end, num, divisible_by=1):
 
     base = np.exp(np.log(end / start) / (num - 1))
     return [_round(start * base ** i) for i in range(num)]
-
-# import math
-#
-# import tensorflow as tf
-# from tensorflow.keras.layers import LeakyReLU, LayerNormalization, MultiHeadAttention, \
-#     Add, Embedding, Layer, Dropout, Reshape, \
-#     Dense, Conv1D, Input, Flatten, Activation, BatchNormalization, LocallyConnected1D, DepthwiseConv1D, DepthwiseConv2D
-# from tensorflow.keras.models import Model
-# from keras import backend as K
-# import numpy as np
-#
-#
-# def make_model(input_size, num_features, num_regions, hic_num, hic_size, one_d_heads):
-#     inputs = Input(shape=(input_size, num_features))
-#     x = inputs
-#     output1d = resnet(x, input_size)
-#     our_resnet = Model(inputs, output1d, name="our_resnet")
-#     outs = our_resnet(inputs)
-#
-#     if hic_num > 0:
-#         # Make hic head
-#         seq_len = output1d.shape[-2]
-#         features = output1d.shape[-1]
-#         hic_input = Input(shape=(seq_len, features))
-#
-#         hx = hic_input
-#         hx = tf.transpose(hx, [0, 2, 1])
-#         hx = Dense(hic_size, activation=tf.nn.gelu, name="hic_mlp_1")(hx)
-#         hx = tf.transpose(hx, [0, 2, 1])
-#         hx = Dense(hic_num, name="hic_mlp_2")(hx)
-#         hx = tf.transpose(hx, [0, 2, 1])
-#
-#         hx = Activation('linear', dtype='float32')(hx)
-#         our_hic = Model(hic_input, hx, name="our_hic")
-#
-#     all_heads = []
-#     if isinstance(one_d_heads, dict):
-#         for key in one_d_heads.keys():
-#             new_head = make_head(len(one_d_heads[key]), num_regions, output1d, "our_" + key)
-#             all_heads.append(new_head(outs))
-#     else:
-#         new_head = make_head(len(one_d_heads), num_regions, output1d, "our_expression")
-#         all_heads.append(new_head(outs))
-#
-#     if hic_num > 0:
-#         all_heads.append(our_hic(outs))
-#
-#     our_model = Model(inputs, all_heads, name="our_model")
-#     # print("\nModel constructed")
-#     print(our_model.summary())
-#     return our_model
-#
-#
-# def make_head(track_num, num_regions, output1d, name):
-#     seq_len = output1d.shape[-2]
-#     features = output1d.shape[-1]
-#     head_input = Input(shape=(seq_len, features))
-#     x = head_input
-#
-#     trim = (x.shape[-2] - num_regions) // 2
-#     x = x[..., trim:-trim, :]
-#
-#     outputs = Conv1D(track_num, kernel_size=1, strides=1, name=name + "_last_conv1d")(x)
-#     outputs = tf.transpose(outputs, [0, 2, 1])
-#     head_output = Activation("linear", dtype='float32')(outputs)
-#     return Model(head_input, head_output, name=name)
-#
-#
-# def resnet(input_x, input_size):
-#     # Initial number of filters
-#     num_filters = 512
-#     mlp_start_block = 6
-#     num_blocks = 10
-#     patchify_val = 4
-#     filter_nums = exponential_linspace_int(num_filters, 2 * num_filters, mlp_start_block, divisible_by=64)
-#     # Patchify layer
-#     x = Conv1D(num_filters,
-#                strides=patchify_val,
-#                kernel_size=patchify_val,
-#                name="patchify")(input_x)
-#     x = LayerNormalization(epsilon=1e-6, dtype=tf.float32)(x)
-#     current_len = input_size // patchify_val
-#     for block in range(num_blocks):
-#         cname = "body_block_" + str(block) + "_"
-#         if block != 0 and block < mlp_start_block:
-#             # Downsample
-#             num_filters = filter_nums[block]
-#             strides = 2
-#             current_len = current_len // strides
-#             x = LayerNormalization(epsilon=1e-6, dtype=tf.float32)(x)
-#             x = Conv1D(num_filters, kernel_size=strides, strides=strides, padding="same", name=cname + "downsample")(x)
-#         y = x
-#         y1 = y
-#         y1 = DepthwiseConv1D(kernel_size=9, name=cname + "depthwise", padding="same")(y1)
-#         # Spatial MLP for long range interactions
-#         if block >= mlp_start_block:
-#             y2 = y
-#             y2 = tf.transpose(y2, [0, 2, 1])
-#             y2 = Dense(4 * current_len, activation=tf.nn.gelu, name=cname + "mlp_1")(y2)
-#             # y2 = Dropout(0.1)(y2)
-#             y2 = Dense(current_len, name=cname + "mlp_2")(y2)
-#             y2 = tf.transpose(y2, [0, 2, 1])
-#             y = y1 + y2
-#         else:
-#             y = y1
-#         y = LayerNormalization(epsilon=1e-6, dtype=tf.float32)(y)
-#         # Pointwise to mix the channels
-#         y = Conv1D(4 * num_filters, kernel_size=1, padding="same", activation=tf.nn.gelu, name=cname + "pointwise_1")(y)
-#         # y = Dropout(0.1)(y)
-#         y = Conv1D(num_filters, kernel_size=1, padding="same", name=cname + "pointwise_2")(y)
-#         x = x + y
-#
-#     x = LayerNormalization(epsilon=1e-6, dtype=tf.float32)(x)
-#     x = Conv1D(4096, kernel_size=1, name="body_output", activation=tf.nn.gelu)(x)
-#     return x
-#
-#
-# @tf.function
-# def fast_mse(y_true, y_pred):
-#     normal_mse = tf.reduce_mean(tf.square(y_true - y_pred))
-#     y_pred_positive = tf.gather_nd(y_pred, tf.where(y_true > 0))
-#     y_true_positive = tf.gather_nd(y_true, tf.where(y_true > 0))
-#     non_zero_mse = tf.reduce_mean(tf.square(y_true_positive - y_pred_positive))
-#     non_zero_mse = tf.where(tf.math.is_nan(non_zero_mse), tf.zeros_like(non_zero_mse), non_zero_mse)
-#     total_loss = normal_mse + 0.1 * non_zero_mse
-#     return total_loss
-#
-#
-# def wrap(input_sequences, output_scores, bs):
-#     with tf.device('cpu:0'):
-#         train_data = tf.data.Dataset.from_tensor_slices((input_sequences, output_scores))
-#         train_data = train_data.batch(bs)
-#         options = tf.data.Options()
-#         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-#         train_data = train_data.with_options(options)
-#         return train_data
-#
-#
-# def wrap_with_hic(input_sequences, output_scores, bs):
-#     with tf.device('cpu:0'):
-#         train_data = tf.data.Dataset.from_tensor_slices(
-#             (input_sequences, {"our_head": output_scores[0], "our_hic": output_scores[1]}))
-#         train_data = train_data.batch(bs)
-#         options = tf.data.Options()
-#         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-#         train_data = train_data.with_options(options)
-#         return train_data
-#
-#
-# def wrap_for_human_training(input_sequences, output_scores, bs):
-#     with tf.device('cpu:0'):
-#         train_data = tf.data.Dataset.from_tensor_slices((input_sequences, output_scores))
-#         train_data = train_data.batch(bs)
-#         options = tf.data.Options()
-#         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-#         train_data = train_data.with_options(options)
-#         return train_data
-#
-#
-# def wrap2(input_sequences, bs):
-#     with tf.device('cpu:0'):
-#         train_data = tf.data.Dataset.from_tensor_slices(input_sequences)
-#         train_data = train_data.batch(bs)
-#         options = tf.data.Options()
-#         options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-#         train_data = train_data.with_options(options)
-#         return train_data
-#
-#
-# def batch_predict(p, model, seqs):
-#     for w in range(0, len(seqs), p.w_step):
-#         print(w, end=" ")
-#         pr = model.predict(wrap2(seqs[w:w + p.w_step], p.predict_batch_size))
-#         p1 = np.concatenate((pr[0], pr[1], pr[2]), axis=1)
-#         # if len(hic_keys) > 0:
-#         #     p2 = p1[0][:, :, p.mid_bin - 1] + p1[0][:, :, p.mid_bin] + p1[0][:, :, p.mid_bin + 1]
-#         #     if w == 0:
-#         #         predictions = p2
-#         #     else:
-#         #         predictions = np.concatenate((predictions, p2), dtype=np.float32)
-#         # else:
-#         if w == 0:
-#             predictions = p1
-#         else:
-#             predictions = np.concatenate((predictions, p1), dtype=np.float16)
-#     return predictions
-#
-#
-# def batch_predict_effect(p, model, seqs1, seqs2):
-#     for w in range(0, len(seqs1), p.w_step):
-#         print(w, end=" ")
-#         pr = model.predict(wrap2(seqs1[w:w + p.w_step], p.predict_batch_size))
-#         p1 = np.concatenate((pr[0], pr[1], pr[2]), axis=1)
-#         pr = model.predict(wrap2(seqs2[w:w + p.w_step], p.predict_batch_size))
-#         p2 = np.concatenate((pr[0], pr[1], pr[2]), axis=1)
-#         # if len(hic_keys) > 0:
-#         #     p2 = p1[0][:, :, p.mid_bin - 1] + p1[0][:, :, p.mid_bin] + p1[0][:, :, p.mid_bin + 1]
-#         #     if w == 0:
-#         #         predictions = p2
-#         #     else:
-#         #         predictions = np.concatenate((predictions, p2), dtype=np.float32)
-#         # else:
-#         effect = np.max(np.abs(p1 - p2), axis=-1)
-#         if w == 0:
-#             predictions = effect
-#         else:
-#             predictions = np.concatenate((predictions, effect), dtype=np.float16)
-#     return predictions
-#
-#
-# # from https://github.com/deepmind/deepmind-research/blob/master/enformer/enformer.py
-# def exponential_linspace_int(start, end, num, divisible_by=1):
-#     """Exponentially increasing values of integers."""
-#
-#     def _round(x):
-#         return int(np.round(x / divisible_by) * divisible_by)
-#
-#     base = np.exp(np.log(end / start) / (num - 1))
-#     return [_round(start * base ** i) for i in range(num)]
